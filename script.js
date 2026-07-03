11081c9b3e696d3196253e8044d3aaa7d7346e69
v1ishalkumar1857-alt <v1ishalkumar1857@gmail.com>
2026-07-03 23:54:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const gift = document.querySelector(".gift");
+const lid = document.querySelector(".lid");
+
+gift.addEventListener("click", ()=>{
+
+lid.style.transform="rotateZ(-35deg) rotateX(80deg)";
+
+});
